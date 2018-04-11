1b4bb201f4a202952b0be0e44b49a3117f6bb522
Doyoung Kim <doyoung970@gmail.com>
2018-04-11 22:03:17 +0900

    final commit

--- src/edu/handong/csee/java/Salesman.java
+++ src/edu/handong/csee/java/Salesman.java
@@ -1,19 +1,20 @@
 package edu.handong.csee.java;
 
-public class Salesman {
-	private String Name;
-	public String getName() {
+public class Salesman { //클래스 salesman
+	private String Name; //함수선언
+	private double Sales; //함수선언
+	
+	public String getName() { //저장된 이름값을 보낸다
 		return Name;
 	}
-	public void setName(String name) {
+	public void setName(String name) { //새로 들어온 이름의 입력값을 this를 이용해서 저장
 		this.Name = name;
 	}
-	public double getSales() {
+	public double getSales() { //저장된 sales값을 보낸다
 		return Sales;
 	}
-	public void setSales(double sales) {
+	public void setSales(double sales) { //새로 들어온 sales의 입력값을 저장
 		this.Sales = sales;
 	}
-	private double Sales;
 	
 }
--- src/edu/handong/csee/java/salesReport.java
+++ src/edu/handong/csee/java/salesReport.java
@@ -2,69 +2,76 @@ package edu.handong.csee.java;
 
 import java.util.Scanner;
 
-public class salesReport {
-	private double highSales;
-	private double averageSales;
-	private Salesman[] team;
+/*제작자: 김도영 21700082
+ * 자바프로그래밍 02분반
+ * 날짜: 04/10
+ * 설명: 이 프로그램은 salesman의 수, salesdata를 입력받아 결과값을 출력해주는 함수입니다.
+ */
+
+public class salesReport { //salesReport 클래스
+	private double highSales; //double변수 highSales를 생성
+	private double averageSales; //double변수 averageSales를 생성
+	private Salesman[] team; //배열 team을 생성
 	
 	public static void main (String[] args) {
-			salesReport analyzer = new salesReport();
+			salesReport analyzer = new salesReport(); //analyzer라는 변수에 salesReport라는 클래스를 인스턴스화
 			
-			analyzer.getData();
-			analyzer.computeAverage();
-			analyzer.computeHighestSales();
-			analyzer.printResults();
-	}
+			analyzer.getData(); //getdata 메소드를 실행
+			analyzer.computeAverage(); //computeAverage 메소드를 실행
+			analyzer.computeHighestSales(); //computeHighestSales 메소드를 실행
+			analyzer.printResults(); //printResult 메소드를 실행
+	} 
 	
-	public void getData() {
+	public void getData() { //getData 메소드
 		System.out.println("enter the number of salesman: ");
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); //스캐너 input 을 생성
 		
-		int numOfSalesman = input.nextInt();
+		int numOfSalesman = input.nextInt(); //numOfSalesman라는 변수에 입력값을 저장
 		
-		team = new Salesman[numOfSalesman];
+		team = new Salesman[numOfSalesman]; //team이라는 배열함수의 크기를  입력값만큼 지정
 		
-		for(int i=0; i <team.length; i++) {
-			System.out.println("enter data for associate number" + (i+1));
-			System.out.println("Name? ");input.nextLine();
-			String name = input.nextLine();
-			System.out.print("sales? ");
-			double sales = input.nextDouble();
-			team[i] = new Salesman();
-			team[i].setName(name);
-			team[i].setSales(sales); 
+		for(int i=0; i <team.length; i++) { //team이라는 배열함수의 길이만큼 for구문을 반복
+			System.out.println("enter data for associate number " + (i+1)); //메세지 출력
+			System.out.println("Name? "); //이름을 적으라는 메세지를 출력
+			String name = input.nextLine(); //name 이라는 변수를 만들어서 입력값을 저장
+			System.out.print("sales? "); //메세지 출력
+			double sales = input.nextDouble(); //sales 이라는 변수를 만들어서 입력값을 저장
+			team[i] = new Salesman(); //저장할 객체를 생성
+			team[i].setName(name); //이름값을 객체에 저장
+			team[i].setSales(sales);  //sales값을 객체에 저장
 			
 		}
 	}
 	
-	public void computeAverage() {
-		double sum =0;
-		for(int i=0; i <team.length;i++) {
-			sum = sum + team[i].getSales();
+	public void computeAverage() { //평균을 구하는 메소드
+		double sum =0; //sum 함수를 초기화
+		for(int i=0; i <team.length;i++) { //team배열의 길이만큼 for구문을 생성
+			sum = sum + team[i].getSales(); //sum함수에 모든 sales값을 더한다
 			
 		}
-		this.averageSales = sum/team.length;
+		this.averageSales = sum/team.length; //averageSales에 평균값을 저장
 	}
 	
-	public void computeHighestSales() {
-		double highestSales =0;
-		for(int i=0; i<team.length;i++) {
-			if(highestSales < team[i].getSales())
-				highestSales = team[i].getSales();
+	public void computeHighestSales() { //가장 높은 sales를 알아내는 메소드
+		double highestSales =0; //0으로 초기화
+		for(int i=0; i<team.length;i++) { //team배열의 길이만큼 for구문을 생성
+			if(highestSales < team[i].getSales()) //highestSales보다 값이 높으면
+				highestSales = team[i].getSales(); //그 값으로 대체
 		}
-		this.highSales=highestSales;
+		this.highSales=highestSales; //가장 높은숫자를 highestSales에 저장
 	}
 	
-	public void printResults() {
-		System.out.println("Average: " + averageSales);
-		System.out.println("salesman who has the highest sales: ");
+	public void printResults() { //결과를 출력하는 함수
+		System.out.println("Average: " + averageSales); //평균값을 출력
+		System.out.println("salesman who has the highest sales: "); //가장 높은 sales를 가진 사람을 출력하기 위한 전 구문
 		
-		for (int i=0;i < team.length; i++) {
-			if(team[i].getSales() == this.highSales) {
-				System.out.println("name: " + team[i].getName());
-				System.out.println("Sales: " + team[i].getSales());
+		for (int i=0;i < team.length; i++) {//team배열의 길이만큼 for구문을 생성
+			if(team[i].getSales() == this.highSales) { //team배열에서 가져온 sales가 가장 높은 sales와 같으면
+				System.out.println("name: " + team[i].getName()); //그 sales를 가진 이름을 출력
+				System.out.println("Sales: " + team[i].getSales()); //그 sales값을 출력
 			}
 		}
 	}
 	
 }
+
